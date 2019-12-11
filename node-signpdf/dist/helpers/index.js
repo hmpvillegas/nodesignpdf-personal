ebc4883b4ca072ed9ba5184b2e031bf37c9afbcf
Hermey Marc P. Villegas <57124141+hmpvillegas@users.noreply.github.com>
2019-12-11 08:48:31 +0800

    Add files via upload
    THE signer1 added in the parameter of plainPlaceholder is to set its coordinates to put the signature via (RECT) and the pdfkitPlaceholder is the reference for the plainPlaceholder since plainPlaceholder is relying on the pdfkitPlaceholder

---
+++ node-signpdf/dist/helpers/index.js
@@ -0,0 +1,55 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "extractSignature", {
+  enumerable: true,
+  get: function () {
+    return _extractSignature.default;
+  }
+});
+Object.defineProperty(exports, "pdfkitAddPlaceholder", {
+  enumerable: true,
+  get: function () {
+    return _pdfkitAddPlaceholder.default;
+  }
+});
+Object.defineProperty(exports, "plainAddPlaceholder", {
+  enumerable: true,
+  get: function () {
+    return _plainAddPlaceholder.default;
+  }
+});
+Object.defineProperty(exports, "removeTrailingNewLine", {
+  enumerable: true,
+  get: function () {
+    return _removeTrailingNewLine.default;
+  }
+});
+
+Object.defineProperty(exports, "getSignatureInfo", {
+  enumerable: true,
+  get: function () {
+    return _getSignatureInfo.default;
+  }
+});
+
+
+
+
+var _extractSignature = _interopRequireDefault(require("./extractSignature"));
+
+var _pdfkitAddPlaceholder = _interopRequireDefault(require("./pdfkitAddPlaceholder"));
+
+var _plainAddPlaceholder = _interopRequireDefault(require("./plainAddPlaceholder"));
+
+var _removeTrailingNewLine = _interopRequireDefault(require("./removeTrailingNewLine"));
+
+var _getSignatureInfo = _interopRequireDefault(require("./getSignatureInfo"));
+
+
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+'This string is added so that jest collects coverage for this file'; // eslint-disable-line
